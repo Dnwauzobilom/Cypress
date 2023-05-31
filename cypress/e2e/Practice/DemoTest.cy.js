07fc8798fbcda5272d2131d96353e9d9ed90d7d4
Dnwauzobilom <dnwauzobilom@gmail.com>
2023-05-31 09:42:34 +0100

    Cypress Practices

---
+++ cypress/e2e/Practice/DemoTest.cy.js
@@ -0,0 +1,16 @@
+const cypress = require('cypress');
+describe("Testing a Demo App", () => {
+
+    it("Test1", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/");
+
+        cy.title().should('eq','OrangeHRM')
+    });
+
+    it("Test2", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/");
+
+        cy.title().should('eq','OrangeHRM123')
+    });
+
+    });
